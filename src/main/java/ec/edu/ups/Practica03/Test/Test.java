46d0eae544e7ddcab81be33711562a167eefc456
USUARIO <USUARIO@DESKTOP-GADRBGL>
2023-05-16 15:19:53 -0500

    Creacion de paquetes

---
+++ src/main/java/ec/edu/ups/Practica03/Test/Test.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package ec.edu.ups.Practica03.Test;
+
+/**
+ *
+ * @author Juan Fernandez
+ */
+public class Test {
+
+    public static void main(String[] args) {
+       
+        
+    }
+}
